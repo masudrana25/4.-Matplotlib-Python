4f65a58792a2fef26f4a3f5652aee8977b7560dc
masudrana25 <mdmasudrana2512000@gmail.com>
2024-01-19 22:19:25 +0600

    first commit , first graph plot, line graph plot full and bar chart plot half done

--- name.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt 
-x = [ 'python' , 'c' , 'c++', 'java'] ; y = [80,70,75,83]
-plt.xlabel('Programming Language', fontsize = 15) 
-plt.ylabel( 'No. of Users', fontsize = 15) 
-plt.title( ' Users vs Language Bar Chart', fontsize = 15)
-color_name = [ 'r', 'y' , 'b' , 'g' ]
-plt.bar(x,y, width = 0.4, color = color_name)
-plt.show()
